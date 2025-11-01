782b47467e29c1ba7242128940207ecc89d67eaa
Ben Belland <benbelland@gmail.com>
2025-11-01 14:12:39 -0700

    refactor animation names

--- rust/src/animationmanager.rs
+++ rust/src/animationmanager.rs
@@ -43,40 +43,40 @@ impl IAnimationPlayer for Manager {
 
 		anim_speed.insert("Idle_unarmed".to_string(), 1.0);
 
-		anim_speed.insert("Pistol_equip".to_string(), 1.4);
-		anim_speed.insert("Pistol_fire".to_string(), 1.8);
-		anim_speed.insert("Pistol_idle".to_string(), 1.0);
-		anim_speed.insert("Pistol_reload".to_string(), 1.0);
-		anim_speed.insert("Pistol_unequip".to_string(), 1.4);
+		anim_speed.insert("Equip_pistol".to_string(), 1.4);
+		anim_speed.insert("Fire_pistol".to_string(), 1.8);
+		anim_speed.insert("Idle_pistol".to_string(), 1.0);
+		anim_speed.insert("Reload_pistol".to_string(), 1.0);
+		anim_speed.insert("Unequip_pistol".to_string(), 1.4);
 
-		anim_speed.insert("Rifle_equip".to_string(), 2.0);
-		anim_speed.insert("Rifle_fire".to_string(), 6.0);
-		anim_speed.insert("Rifle_idle".to_string(), 1.0);
-		anim_speed.insert("Rifle_reload".to_string(), 1.45);
-		anim_speed.insert("Rifle_unequip".to_string(), 2.0);
+		anim_speed.insert("Equip_rifle".to_string(), 2.0);
+		anim_speed.insert("Fire_rifle".to_string(), 6.0);
+		anim_speed.insert("Idle_rifle".to_string(), 1.0);
+		anim_speed.insert("Reload_rifle".to_string(), 1.45);
+		anim_speed.insert("Unequip_rifle".to_string(), 2.0);
 
-		anim_speed.insert("Knife_equip".to_string(), 1.0);
-		anim_speed.insert("Knife_fire".to_string(), 1.35);
-		anim_speed.insert("Knife_idle".to_string(), 1.0);
-		anim_speed.insert("Knife_unequip".to_string(), 1.0);
+		anim_speed.insert("Equip_sword".to_string(), 1.0);
+		anim_speed.insert("Fire_sword".to_string(), 1.35);
+		anim_speed.insert("Idle_knife".to_string(), 1.0);
+		anim_speed.insert("Unequip_sword".to_string(), 1.0);
 		let mut state = HashMap::new();
-		state.insert("Idle_unarmed".to_string(), vec!["Knife_equip".to_string(), "Pistol_equip".to_string(), "Rifle_equip".to_string(), "Idle_unarmed".to_string()]);
+		state.insert("Idle_unarmed".to_string(), vec!["Equip_sword".to_string(), "Equip_pistol".to_string(), "Equip_rifle".to_string(), "Idle_unarmed".to_string()]);
 
-		state.insert("Pistol_equip".to_string(), vec!["Pistol_idle".to_string()]);
-		state.insert("Pistol_fire".to_string(), vec!["Pistol_idle".to_string()]);
-		state.insert("Pistol_idle".to_string(), vec!["Pistol_fire".to_string(), "Pistol_reload".to_string(), "Pistol_unequip".to_string(), "Pistol_idle".to_string()]);
-		state.insert("Pistol_reload".to_string(), vec!["Pistol_idle".to_string()]);
-		state.insert("Pistol_unequip".to_string(), vec!["Idle_unarmed".to_string()]);
+		state.insert("Equip_pistol".to_string(), vec!["Idle_pistol".to_string()]);
+		state.insert("Fire_pistol".to_string(), vec!["Idle_pistol".to_string()]);
+		state.insert("Idle_pistol".to_string(), vec!["Fire_pistol".to_string(), "Reload_pistol".to_string(), "Unequip_pistol".to_string(), "Idle_pistol".to_string()]);
+		state.insert("Reload_pistol".to_string(), vec!["Idle_pistol".to_string()]);
+		state.insert("Unequip_pistol".to_string(), vec!["Idle_unarmed".to_string()]);
 
-		state.insert("Rifle_equip".to_string(), vec!["Rifle_idle".to_string()]);
-		state.insert("Rifle_fire".to_string(), vec!["Rifle_idle".to_string()]);
-		state.insert("Rifle_idle".to_string(), vec!["Rifle_fire".to_string(), "Rifle_reload".to_string(), "Rifle_unequip".to_string(), "Rifle_idle".to_string()]);
-		state.insert("Rifle_reload".to_string(), vec!["Rifle_idle".to_string()]);
-		state.insert("Rifle_unequip".to_string(), vec!["Idle_unarmed".to_string()]);
-		state.insert("Knife_equip".to_string(), vec!["Knife_idle".to_string()]);
-		state.insert("Knife_fire".to_string(), vec!["Knife_idle".to_string()]);
-		state.insert("Knife_idle".to_string(), vec!["Knife_fire".to_string(), "Knife_unequip".to_string(), "Knife_idle".to_string()]);
-		state.insert("Knife_unequip".to_string(), vec!["Idle_unarmed".to_string()]);
+		state.insert("Equip_rifle".to_string(), vec!["Idle_rifle".to_string()]);
+		state.insert("Fire_rifle".to_string(), vec!["Idle_rifle".to_string()]);
+		state.insert("Idle_rifle".to_string(), vec!["Fire_rifle".to_string(), "Reload_rifle".to_string(), "Unequip_rifle".to_string(), "Idle_rifle".to_string()]);
+		state.insert("Reload_rifle".to_string(), vec!["Idle_rifle".to_string()]);
+		state.insert("Unequip_rifle".to_string(), vec!["Idle_unarmed".to_string()]);
+		state.insert("Equip_sword".to_string(), vec!["Idle_knife".to_string()]);
+		state.insert("Fire_sword".to_string(), vec!["Idle_knife".to_string()]);
+		state.insert("Idle_knife".to_string(), vec!["Fire_sword".to_string(), "Unequip_sword".to_string(), "Idle_knife".to_string()]);
+		state.insert("Unequip_sword".to_string(), vec!["Idle_unarmed".to_string()]);
 
 		Self {
 			current_state: "".to_string(),
@@ -146,37 +146,37 @@ impl Manager {
 			//pass
 		}
 		// KNIFE transitions
-		else if self.current_state == "Knife_equip".to_string() {
-			self.set_animation("Knife_idle".to_string());
-		}else if self.current_state == "Knife_idle".to_string() {
+		else if self.current_state == "Equip_sword".to_string() {
+			self.set_animation("Idle_knife".to_string());
+		}else if self.current_state == "Idle_knife".to_string() {
 			//pass
-		}else if self.current_state == "Knife_fire".to_string() {
-			self.set_animation("Knife_idle".to_string());
-		}else if self.current_state == "Knife_unequip".to_string() {
+		}else if self.current_state == "Fire_sword".to_string() {
+			self.set_animation("Idle_knife".to_string());
+		}else if self.current_state == "Unequip_sword".to_string() {
 			self.set_animation("Idle_unarmed".to_string());
 		}
 		// PISTOL transitions
-		else if self.current_state == "Pistol_equip".to_string() {
-			self.set_animation("Pistol_idle".to_string());
-		}else if self.current_state == "Pistol_idle".to_string() {
+		else if self.current_state == "Equip_pistol".to_string() {
+			self.set_animation("Idle_pistol".to_string());
+		}else if self.current_state == "Idle_pistol".to_string() {
 			//pass
-		}else if self.current_state == "Pistol_fire".to_string() {
-			self.set_animation("Pistol_idle".to_string());
-		}else if self.current_state == "Pistol_unequip".to_string(){
+		}else if self.current_state == "Fire_pistol".to_string() {
+			self.set_animation("Idle_pistol".to_string());
+		}else if self.current_state == "Unequip_pistol".to_string(){
 			self.set_animation("Idle_unarmed".to_string());
-		}else if self.current_state == "Pistol_reload".to_string(){
-			self.set_animation("Pistol_idle".to_string());
+		}else if self.current_state == "Reload_pistol".to_string(){
+			self.set_animation("Idle_pistol".to_string());
 		// RIFLE transitions
-		}else if self.current_state == "Rifle_equip".to_string() {
-			self.set_animation("Rifle_idle".to_string());
-		}else if self.current_state == "Rifle_idle".to_string() {
+		}else if self.current_state == "Equip_rifle".to_string() {
+			self.set_animation("Idle_rifle".to_string());
+		}else if self.current_state == "Idle_rifle".to_string() {
 			//pass
-		}else if self.current_state == "Rifle_fire".to_string() {
-			self.set_animation("Rifle_idle".to_string());
-		}else if self.current_state == "Rifle_unequip".to_string() {
+		}else if self.current_state == "Fire_rifle".to_string() {
+			self.set_animation("Idle_rifle".to_string());
+		}else if self.current_state == "Unequip_rifle".to_string() {
 			self.set_animation("Idle_unarmed".to_string());
-		}else if self.current_state == "Rifle_reload".to_string(){
-			self.set_animation("Rifle_idle".to_string());
+		}else if self.current_state == "Reload_rifle".to_string(){
+			self.set_animation("Idle_rifle".to_string());
 		}
 	}
 }
